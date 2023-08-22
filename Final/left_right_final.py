9bcce52613855831f39813b9d23fd75cb7255252
SiHyun1120 <101866090+SiHyun1120@users.noreply.github.com>
2023-08-23 08:15:55 +0900

    Update left_right_final.py
    arduino to jetson complete
    
    only need is [jetson to arduino]

--- Final/left_right_final.py
+++ Final/left_right_final.py
@@ -15,10 +15,11 @@ from sklearn.linear_model import LogisticRegression, RidgeClassifier
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 
+
 mp_drawing = mp.solutions.drawing_utils 
 mp_pose = mp.solutions.pose 
 
-df = pd.read_csv('coords_spine_final.csv')
+df = pd.read_csv('coords_spine_final2.csv')
 
 X = df.drop('spine',axis=1) #shol을 제외한 입력 feature만 남김.
 y = df['spine']
@@ -37,12 +38,6 @@ for algo,pipeline in pipelines.items():
     model = pipeline.fit(X_train,y_train)
     fit_models[algo] = model
 
-for algo, model in fit_models.items():
-    yhat = model.predict(X_test)
-    print(algo,accuracy_score(y_test.values,yhat),
-    precision_score(y_test.values,yhat,average=None),
-    recall_score(y_test.values,yhat,average=None))
-
 yhat = fit_models['rf'].predict(X_test)
 
 with open('Spine.pkl','wb') as f:
@@ -56,14 +51,12 @@ for val in range(1,13+1):
     landmarks +=['x{}'.format(val),'y{}'.format(val),'z{}'.format(val),'v{}'.format(val)]
 
 
-port = "COM6"
+port = "/dev/ttyACM1"
 brate = 9600 
 cmd = 't'
 
 seri = serial.Serial(port, baudrate = brate, timeout = None)
 print(seri.name)
-
-seri.write(cmd.encode())
 
 counter =0
 
@@ -103,14 +96,19 @@ with mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5) as p
             if seri.in_waiting != 0 :
                 content1 = seri.readline().decode('ascii')
                 X['StnX'] = float(content1[:-2])
+
                 content2 = seri.readline().decode('ascii')
                 X['StnY'] = float(content2[:-2])
+
                 content3 = seri.readline().decode('ascii')
                 X['X0'] = float(content3[:-2])
+
                 content4 = seri.readline().decode('ascii')
                 X['Y0'] = float(content4[:-2])
+
                 content5 = seri.readline().decode('ascii')
                 X['X'] = float(content5[:-2])
+
                 content6 = seri.readline().decode('ascii')
                 X['Y'] = float(content6[:-2])
 
@@ -126,6 +124,8 @@ with mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5) as p
                 current_stage = 'right'
                 counter +=1
 
+            print(body_language_prob)
+            print("\n")
 
             cv2.rectangle(image,(0,0),(250,60),(245,117,16),-1) 
 
@@ -155,3 +155,5 @@ with mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5) as p
 
     cap.release() 
     cv2.destroyAllWindows() 
+
+    print(X)
